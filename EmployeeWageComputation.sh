64f831f130486325bce3f49fef23bd52123986bd
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-23 17:08:48 +0530

    ADDED: function for employee working hours

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -9,11 +9,9 @@ MAX_WORKING_HOURS_IN_MONTH=100
 #Variable
 totalEmployeeHours=0
 totalWorkingDays=0
-
-while [[ $totalEmployeeHours -lt $MAX_WORKING_HOURS_IN_MONTH && $totalWorkingDays -lt $WORKING_DAYS_OF_EMPLOYEE ]]
-do
-	((totalWorkingDays++))
-	employeeCheck=$((RANDOM%3))
+function getWorkHoursOfEmployee()
+{
+	local employeeCheck=$1
 	case $employeeCheck in
 		$EMPLOYEE_WORKING_FULLTIME)
 			employeeHours=8
@@ -25,6 +23,13 @@ do
 			employeeHours=0
 			;;
 	esac
+	echo $employeeHours
+}
+while [[ $totalEmployeeHours -lt $MAX_WORKING_HOURS_IN_MONTH && $totalWorkingDays -lt $WORKING_DAYS_OF_EMPLOYEE ]]
+do
+	((totalWorkingDays++))
+	employeeCheck=$(( RANDOM%3 ))
+	employeeHours="$( getWorkHoursOfEmployee $employeeCheck )"
 	totalEmployeeHours=$(($totalEmployeeHours + $employeeHours))
 done
 totalSalary=$(($totalEmployeeHours*$EMPLOYEE_RATE_PER_HOUR))
