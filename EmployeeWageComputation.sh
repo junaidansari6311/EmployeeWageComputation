0f9dd209160767b908e1b21a52470113d32a5a1d
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-23 12:49:01 +0530

    ADDED:Calculate daily employee wage

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -2,10 +2,12 @@
 echo "Welcome to Employee Wage Computation"
 #Constant
 EMPLOYEE_IS_PRESENT=1
+EMPLOYEE_RATE_PER_HOUR=20
 employeeCheck=$((RANDOM%2))
 if [ $employeeCheck -eq $EMPLOYEE_IS_PRESENT ]
 then
-	echo "Employee is Present"
+	employeeHours=8
+	employeeSalary=$(($employeeHours*$EMPLOYEE_RATE_PER_HOUR))
 else
-	echo "Employee is Absent"
+	employeeSalary=0
 fi
