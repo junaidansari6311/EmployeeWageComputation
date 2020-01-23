fc64b572ebebd033217ad11544ad4a2c0d5712e7
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-23 15:00:26 +0530

    ADDED: forloop and variables for calculating wages for month

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -4,19 +4,25 @@ echo "Welcome to Employee Wage Computation"
 EMPLOYEE_WORKING_PARTTIME=1
 EMPLOYEE_WORKING_FULLTIME=2
 EMPLOYEE_RATE_PER_HOUR=20
+WORKING_DAYS_OF_EMPLOYEE=20
+#Variable
+totalSalary=0
 
-employeeCheck=$((RANDOM%3))
-
-case $employeeCheck in
-	$EMPLOYEE_WORKING_FULLTIME)
-		employeeHours=8
-		;;
-	$EMPLOYEE_WORKING_PARTTIME)
-		employeeHours=4
-		;;
-	*)
-		employeeHours=0
-		;;
-esac
+for (( day=1; day<=$WORKING_DAYS_OF_EMPLOYEE; day++ ))
+do
+	employeeCheck=$((RANDOM%3))
+	case $employeeCheck in
+		$EMPLOYEE_WORKING_FULLTIME)
+			employeeHours=8
+			;;
+		$EMPLOYEE_WORKING_PARTTIME)
+			employeeHours=4
+			;;
+		*)
+			employeeHours=0
+			;;
+	esac
 
 employeeSalary=$(($employeeHours*$EMPLOYEE_RATE_PER_HOUR))
+totalSalary=$(($totalSalary + $employeeSalary))
+done
